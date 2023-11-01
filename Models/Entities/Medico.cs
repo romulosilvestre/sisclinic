221990f838bbeda6bd1e8c3908ecb922a749c4c7
Rogerio <rogerio.santana@df.docente.senai.br>
2023-11-01 17:52:35 -0300

    layout

---
+++ Models/Entities/Medico.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaWeb.Models.Entities
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        
+    }
+}
