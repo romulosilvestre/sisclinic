d20cbe6de4ca12d338cb462a0b0b98f7a20a6841
romulosilvestre <rogerio.santana@df.docente.senai.br>
2023-11-01 18:13:57 -0300

    Medico

--- Models/Entities/Medico.cs
+++ Models/Entities/Medico.cs
@@ -9,6 +9,7 @@ namespace SistemaWeb.Models.Entities
     {
         public int Id { get; set; }
         public string? Nome { get; set; }
+        public string CRM { get; set; } = string.Empty;
         
     }
 }
